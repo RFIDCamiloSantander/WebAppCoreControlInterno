5719d06033ea7fa9994e144d1dd4c814aef91024
Camilo Santander <demon_camilo@hotmail.com>
2022-06-08 22:23:34 -0400

    Avance en CRUDs

---
+++ WebAppCoreControlInterno/Controllers/ElementoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppCoreControlInterno.Models;
+
+namespace WebAppCoreControlInterno.Controllers
+{
+    public class ElementoController : Controller
+    {
+
+        private readonly ControlInternoContext _context;
+
+
+        public ElementoController(ControlInternoContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAppCoreControlInterno/Controllers/EstadoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppCoreControlInterno.Models;
+
+namespace WebAppCoreControlInterno.Controllers
+{
+    public class EstadoController : Controller
+    {
+
+        //Se crea la variable para el conexto (BD)
+        private readonly ControlInternoContext _context;
+
+        //Se asigna el conexto(BD) a la variable
+        public EstadoController(ControlInternoContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult IndexEstado()
+        {
+            return View();
+        }
+    }
+}
