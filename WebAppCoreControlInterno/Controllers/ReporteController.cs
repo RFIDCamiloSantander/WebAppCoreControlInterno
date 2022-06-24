93d8185e6bf4a472e6b8dc521c005881aa576d0a
Camilo Santander <demon_camilo@hotmail.com>
2022-06-24 00:06:34 -0400

    WIP reportes

---
+++ WebAppCoreControlInterno/Controllers/ReporteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WebAppCoreControlInterno.Models;
+
+namespace WebAppCoreControlInterno.Controllers
+{
+    public class ReporteController : Controller
+    {
+        private readonly ControlInternoContext _context;
+
+        public ReporteController(ControlInternoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> IndexReporte()
+        {
+            //ViewBag.Elementos = await _context.Elementos.ToListAsync();
+            return View();
+        }
+    }
+}
